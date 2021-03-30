8bc1ea38ecfc13b65965ae0b24b5a144a36ad17a
mezz <mezzlasha@gmail.com>
2021-03-30 16:08:23 +0000

    [crdroid][11.0] TypeClock: Introduce ShapeShift lockscreen clock
    Very very very heavily inspired by OOS. pretty much OOS. it's OOS.
    
    MezzLasha: Fix ShapeShift clock on 24h format
    neobuddy89: Fix up preview for ShapeShift clock
    
    Change-Id: I3767b3913956f394ac20b64817346fbbea0b0756

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -151,6 +151,7 @@ public final class ClockManager {
 
         addBuiltinClock(() -> new DefaultClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new TypeClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new ShapeShiftClockController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
